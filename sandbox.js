e2a32c6a8d777659643f4d3b01f4c6fad87fbe0f
developer deepu <65069470+sudeepdeep@users.noreply.github.com>
2021-08-25 00:23:27 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,26 @@
 const message = document.querySelector('form');
 const usertext = document.querySelector('.usertext');
 const user = document.querySelector('.user');
-const result = document.querySelector('.resulttext');
-const chattext = document.querySelector('.chattext');
+const chattext = document.querySelector('.chattext')
+const chatheading = document.querySelector('.chatheading');
 
 
-const chatdisplay = (itemname, ingridients, process) => {
+
+
+const APP_ID = "790ce370";
+const APP_key = "55d3194c4890b23fa1f23f5b66f30e95";
+let ingre = ""
+const itemcard = (stri, url, itemlogo, heading) => {
+    stri.forEach(elem => {
+        ingre +=elem.text;
+        ingre += ", "
+    })
+
     chattext.classList.remove('d-none');
-    let res = `<p>ItemName: ${itemname}<br> Ingridients: ${ingridients}<br> Process: ${process} </p>`
+    let head = `<h3>${heading}</h3> <br> <img src = "${itemlogo}" > <br> <p>Ingredients: ${ingre}</p><br><a href = "${url}" target = "_blank">see more..</a>`;
 
-    result.innerHTML += res;
-}
+    chatheading.innerHTML += head; 
 
-const chatreply = (text) => {
-   fetch('data.json').then(data => {
-    // let datum = JSON.stringify(data);
-    return data.json();
-   }).then(datum => {
-       datum.forEach(element => {
-        if(element.itemname === text){
-            chatdisplay(element.itemname, element.ingridients, element.process);
-        }
-       });
-   }).catch(err => {console.log(err)})
 }
 
 
@@ -30,14 +28,33 @@ const display = (text) => {
     user.classList.remove('d-none');
     let html = `<p>${text}</p>`;
     usertext.innerHTML += html;
-    chatreply(text);
+
+    const baseURL = `https://api.edamam.com/search?q=${text}&app_id=${APP_ID}&app_key=${APP_key}`;
+    const response =  fetch(baseURL).then(data => {
+    return data.json();
+    }).then(datum => {
+    const ingredients = datum.hits[0].recipe.ingredients;
+    const url = datum.hits[0].recipe.url;
+    const heading = datum.hits[0].recipe.label;
+    const itemlogo = datum.hits[0].recipe.image;
+    message.message.value = ""
+    itemcard(ingredients, url, itemlogo, heading);
+
+}).catch(err => {
+    console.log(err);
+})
+   
 }
 
 
-
-message.addEventListener('submit', e =>{
+message.addEventListener('submit', e => {
     e.preventDefault();
-    display(message.message.value);
+    let itemtext = message.message.value;
+    display(itemtext.toLowerCase());
     message.message.value = ""
-    // console.log(message.message.value);
+
 })
+
+
+
+
